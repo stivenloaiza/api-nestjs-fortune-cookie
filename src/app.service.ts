5d4a7528952e93eae0db85cc816b519829f21817
unknown <loaizadeveloper@gmail.com>
2024-06-26 13:58:27 -0500

    Get fortune cookie OK

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,20 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getCookie(user: string): string {
+    const listFortunePhrases: string[] = [
+      'El éxito llegará cuando menos lo esperes.',
+      'Un pequeño cambio puede llevar a grandes resultados.',
+      'La perseverancia es la clave del éxito.',
+      'Tu creatividad te llevará lejos en la vida.',
+      'Hoy es un buen día para tomar nuevas decisiones.',
+      'La paciencia es una virtud que será recompensada.',
+      'No temas arriesgarte; las mejores oportunidades están fuera de tu zona de confort.',
+      'Un viaje de mil millas comienza con un solo paso.',
+      'El mejor predictor del futuro es el presente.',
+      'Confía en tu intuición; es más sabia de lo que crees.',
+    ];
+    const fortuneNumber = Math.floor(Math.random() * listFortunePhrases.length);
+    return `${user}, ${listFortunePhrases[fortuneNumber]}`;
   }
 }
