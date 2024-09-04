8833a430a3c5aaca9fdbd627d6b73fac1e0af960
unknown <loaizadeveloper@gmail.com>
2024-09-04 15:40:26 -0500

    Pipeline linter ready v2

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   let hola;
+  let hola2;
   await app.listen(3001);
 }
 bootstrap();
